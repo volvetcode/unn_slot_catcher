0d55ebaf4adc37652fb3d8b612ccbe8cc8b6a7d9
volvet <darkvolvet@icloud.com>
2025-07-02 18:02:56 +0300

    added command line arguments support, made the statistics runtime look better

--- src/main.py
+++ src/main.py
@@ -1,25 +1,49 @@
 #!/usr/bin/env python3
 import logging
 import time
+import sys
 
 import catcher.constants as const
 from catcher.catcher import Catcher
 from catcher.catcher_report import CatcherReport
 
 
-def main():
-    # teardown=False = debug mode
+def main(argv):
+    duration_hours = 8
+    psychologist_index = 0
+
+    n_argv = len(argv)
+    if n_argv < 4:
+        if n_argv > 1:
+            try:
+                duration_hours = float(sys.argv[1])
+            except:
+                logging.warning('invalid duration, using default 8 hours')
+        
+        if n_argv > 2:
+            try:
+                psych_index = int(sys.argv[2])
+            except:
+                logging.warning('invalid psychologist, we will look for psychologist №0')
+        
+    else:
+        logging.warning("too many arguments. running in default mode")
+
+    logging.info(f'the script will run for {duration_hours} hours')
+    logging.info(f'we are looking for {const.PSYCHOLOGISTS[psych_index]}')
+
     try:
+        # teardown=False = debug mode
         with Catcher(teardown=True) as bot:
             bot.login()
             bot.next_page()
 
-            # 6 hours. 60sec * 60mins * 8hours
-            target_time = 60 * 60 * 8
+            # hours to seconds 
+            target_time = 60 * 60 * duration_hours
             time_start = time.time()
 
             found_slot = False
-            psych = const.PSYCHOLOGISTS[0]
+            psych = const.PSYCHOLOGISTS[psychologist_index]
             while (time.time() - time_start) < target_time:
                 if bot.search_for_a_slot(psych):
                     found_slot = True
@@ -36,10 +60,10 @@ def main():
                 )
 
             total_time = (time.time() - time_start) / 60
-            logging.info(f"Statistics: runtime={total_time}mins")
+            logging.info(f"Statistics: runtime={total_time:.0f}mins")
     except Exception as e:
         print(e)
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
